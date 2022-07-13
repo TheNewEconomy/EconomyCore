4d092e46c51963e6135a5779d32df9467c56dd49
creatorfromhell <daniel.viddy@gmail.com>
2022-07-12 22:26:28 -0400

    Currency Type files.

---
+++ Core/src/net/tnemc/core/currency/type/MixedType.java
@@ -0,0 +1,64 @@
+package net.tnemc.core.currency.type;
+/*
+ * The New Economy Minecraft Server Plugin
+ *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/ or send a letter to
+ * Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+ */
+
+import net.tnemc.core.account.Account;
+import net.tnemc.core.account.holdings.HoldingsType;
+import net.tnemc.core.currency.Currency;
+import net.tnemc.core.currency.CurrencyType;
+
+import java.math.BigDecimal;
+
+/**
+ * Represents a currency type that is a mixture of items and virtual.
+ *
+ * @author creatorfromhell
+ * @since 0.1.2.0
+ */
+public class MixedType implements CurrencyType {
+  /**
+   * @return The name of this currency type. Examples: Virtual, Item
+   */
+  @Override
+  public String name() {
+    return "mixed";
+  }
+
+  /**
+   * Used to get the holdings for a specific account from this currency type.
+   *
+   * @param account  The uuid of the account.
+   * @param type
+   * @param region   The name of the region involved. This is usually a world, but could be something
+   *                 else such as a world guard region name/identifier.
+   * @param currency The instance of the currency to use.
+   *
+   * @return The holdings for the specific account.
+   */
+  @Override
+  public BigDecimal getHoldings(Account account, HoldingsType type, String region, Currency currency) {
+    return null;
+  }
+
+  /**
+   * Used to set the holdings for a specific account.
+   *
+   * @param account
+   * @param type
+   * @param region   The name of the region involved. This is usually a world, but could be something
+   *                 else such as a world guard region name/identifier.
+   * @param currency The instance of the currency to use.
+   * @param amount   The amount to set the player's holdings to.
+   *
+   * @return True if the holdings have been set, otherwise false.
+   */
+  @Override
+  public boolean setHoldings(Account account, HoldingsType type, String region, Currency currency, BigDecimal amount) {
+    return false;
+  }
+}
