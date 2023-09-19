964fbb656a8189e51ed10b5dd8b1b8bd6ae5af97
creatorfromhell <daniel.viddy@gmail.com>
2023-09-18 21:41:43 -0400

    Add changelog files

--- Core/src/net/tnemc/core/TNECore.java
+++ Core/src/net/tnemc/core/TNECore.java
@@ -86,8 +86,8 @@ public abstract class TNECore {
    */
   public static final String coreURL = "https://tnemc.net/files/module-version.xml";
   public static final Pattern UUID_MATCHER_PATTERN = Pattern.compile("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})");
-  public static final String version = "0.1.2.0";
-  public static final String build = "Pre-29";
+  public static final String version = "0.1.2.4";
+  public static final String build = "Pre-1";
 
   /* Core non-final variables utilized within TNE as settings */
   protected File directory;
