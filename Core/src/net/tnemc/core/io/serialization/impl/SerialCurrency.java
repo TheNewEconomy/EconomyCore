625d9b20c300cdcdbb16158d4381809f49feb768
creatorfromhell <daniel.viddy@gmail.com>
2023-12-31 09:55:07 -0500

    Fix issue where the currency is saved to incorrect file based on identifier.

--- Core/src/net/tnemc/core/io/serialization/impl/SerialCurrency.java
+++ Core/src/net/tnemc/core/io/serialization/impl/SerialCurrency.java
@@ -17,6 +17,7 @@ package net.tnemc.core.io.serialization.impl;
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import net.tnemc.core.TNECore;
 import net.tnemc.core.currency.Currency;
 import net.tnemc.core.currency.Denomination;
 import net.tnemc.core.currency.item.ItemCurrency;
