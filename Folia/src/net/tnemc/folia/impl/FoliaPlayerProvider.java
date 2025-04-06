e22dc7c935ad2560a21f7238c21f7707c6cdb341
Daniel V. <daniel.viddy@gmail.com>
2025-04-06 03:00:08 -0400

    Update pom.

--- Folia/src/net/tnemc/folia/impl/FoliaPlayerProvider.java
+++ Folia/src/net/tnemc/folia/impl/FoliaPlayerProvider.java
@@ -183,11 +183,6 @@ public class FoliaPlayerProvider extends FoliaPlayer implements PlayerProvider {
     return new FoliaInventoryProvider(identifier(), PaperCore.instance().getPlugin());
   }
 
-  @Override
-  public List<String> getEffectivePermissions() {
-    return List.of();
-  }
-
   /**
    * Used to determine if this player has the specified permission node.
    *
@@ -224,7 +219,7 @@ public class FoliaPlayerProvider extends FoliaPlayer implements PlayerProvider {
       return;
     }
 
-    try(BukkitAudiences provider = BukkitAudiences.create(PaperCore.instance().getPlugin())) {
+    try(final BukkitAudiences provider = BukkitAudiences.create(PaperCore.instance().getPlugin())) {
       MessageHandler.translate(messageData, player.getUniqueId(), provider.sender(player.getPlayer()));
     }
   }
@@ -234,11 +229,12 @@ public class FoliaPlayerProvider extends FoliaPlayer implements PlayerProvider {
     return player;
   }
 
+
   public static FoliaPlayerProvider find(final String identifier) {
 
     try {
       return new FoliaPlayerProvider(Bukkit.getOfflinePlayer(UUID.fromString(identifier)));
-    } catch(Exception ignore) {
+    } catch(final Exception ignore) {
       return new FoliaPlayerProvider(Bukkit.getOfflinePlayer(identifier));
     }
   }
