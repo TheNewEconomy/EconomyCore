38e16647b2551437c2146ed63942a5a59c4e3ee7
Daniel V. <daniel.viddy@gmail.com>
2025-08-09 00:26:34 -0400

    Update changelog for 0.1.4.0 release.

--- Velocity/src/net/tnemc/velocity/VelocityCore.java
+++ Velocity/src/net/tnemc/velocity/VelocityCore.java
@@ -36,7 +36,7 @@ import java.util.logging.Logger;
  * @author creatorfromhell
  * @since 0.1.2.0
  */
-@Plugin(id = "tne_velocity", name = "The New Economy Velocity", version = "0.1.3.5",
+@Plugin(id = "tne_velocity", name = "The New Economy Velocity", version = "0.1.4.0",
         url = "https://tnemc.net", description = "A bridge for TheNewEconomy plugin.", authors = { "creatorfromhell" })
 public class VelocityCore {
 
